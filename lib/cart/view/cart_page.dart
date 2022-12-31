69069a4f49eda6dac5c939e2e3014ccbb3b201d5
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2022-12-31 20:50:49 +0700

    added a checkout button
    Added a checkout button that reacts to price changes and item selection in the cart.
    Currently, checkout button won't take you to the checkout page.

--- lib/cart/view/cart_page.dart
+++ lib/cart/view/cart_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mini_marketplace_app/cart/bloc/cart_bloc.dart';
+import 'package:mini_marketplace_app/cart/model/cart_item.dart';
 import 'package:mini_marketplace_app/cart/view/view.dart';
+import 'package:mini_marketplace_app/cart/widgets/widgets.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -9,6 +13,23 @@ class CartPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Keranjang"), centerTitle: true),
       body: const CartView(),
+      bottomNavigationBar: BlocBuilder<CartBloc, CartState>(
+        builder: (context, state) {
+          if (state.cartItemList.isEmpty) return const SizedBox(height: 100);
+
+          List<CartItem> cartItems = context.read<CartBloc>().state.cartItemList;
+
+          int totalSelectedItem = cartItems.where((item) => item.selected).length;
+
+          int totalSelectedPrice = totalSelectedItem > 0
+              ? cartItems.map((item) {
+                  return item.selected ? item.cartItem.price * item.count : 0;
+                }).reduce((item1, item2) => item1 + item2)
+              : 0;
+
+          return CheckoutButton(totalSelectedItem: totalSelectedItem, totalSelectedPrice: totalSelectedPrice);
+        },
+      ),
     );
   }
 }
--- lib/cart/widgets/widgets.dart
+++ lib/cart/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'package:mini_marketplace_app/cart/widgets/cart_item_card.dart';
+export 'package:mini_marketplace_app/cart/widgets/checkout_button.dart';
