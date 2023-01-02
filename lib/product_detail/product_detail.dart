d58420d074188eef6808228a10f3f1792b6fca80
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-01-03 00:42:05 +0700

    add product detail screen

---
+++ lib/product_detail/product_detail.dart
@@ -0,0 +1,2 @@
+export 'package:mini_marketplace_app/product_detail/view/view.dart';
+export 'package:mini_marketplace_app/product_detail/widgets/widgets.dart';
---
+++ lib/product_detail/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'package:mini_marketplace_app/product_detail/widgets/product_image.dart';
+export 'package:mini_marketplace_app/product_detail/widgets/product_title.dart';
