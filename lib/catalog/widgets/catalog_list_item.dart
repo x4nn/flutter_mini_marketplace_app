f60711cb31a7c1948c0fe6b5943ae22657f78160
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-01-03 00:29:24 +0700

    remove 'add to cart' button in catalog item
    Fix inkwell splash behind product image

--- lib/catalog/widgets/catalog_list_item.dart
+++ lib/catalog/widgets/catalog_list_item.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:mini_marketplace_app/cart/bloc/cart_bloc.dart';
 import 'package:mini_marketplace_app/catalog/model/models.dart';
+import 'package:mini_marketplace_app/product_detail/product_detail.dart';
 
 class CatalogListItem extends StatelessWidget {
   const CatalogListItem({Key? key, required this.item}) : super(key: key);
@@ -12,73 +10,73 @@ class CatalogListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Material(
-      elevation: 2,
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(15),
-      child: InkWell(
-        onTap: () {},
-        borderRadius: BorderRadius.circular(15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(item.image),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.blue,
-                ),
-                child: const Center(child: Text('Gambar barang')),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(item.title, style: textTheme.titleLarge),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(Icons.star, color: Colors.yellow),
-                            Text('${item.rating}', style: textTheme.bodyMedium),
-                          ],
-                        ),
-                        Text('Rp${item.price}', style: textTheme.titleMedium),
-                      ],
+    return Stack(
+      children: [
+        Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          margin: const EdgeInsets.all(0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(item.image),
+                      fit: BoxFit.cover,
                     ),
-                  ],
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.blue,
+                  ),
                 ),
               ),
-            ),
-            OutlinedButton.icon(
-              onPressed: () {
-                context.read<CartBloc>().add(CartEventAddItem(item: item, count: 1));
-                Fluttertoast.cancel().whenComplete(() {
-                  Fluttertoast.showToast(msg: '"${item.title}" telah dimasukkan ke keranjang');
-                });
-              },
-              icon: const Icon(Icons.add_shopping_cart),
-              label: const Text("Tambah ke Keranjang"),
-              style: OutlinedButton.styleFrom(
-                side: const BorderSide(color: Colors.transparent),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(item.title, style: textTheme.titleLarge),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Rp${item.price}', style: textTheme.titleMedium),
+                          Row(
+                            children: [
+                              const Icon(Icons.star, color: Colors.yellow),
+                              Text('${item.rating}', style: textTheme.bodyMedium),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+        Positioned.fill(
+          child: Material(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(15),
+            child: InkWell(
+              splashColor: const Color.fromARGB(101, 33, 149, 243),
+              onTap: () {
+                final route = MaterialPageRoute(
+                  builder: (context) => ProductDetailPage(item: item),
+                );
+                Navigator.of(context).push(route);
+              },
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
